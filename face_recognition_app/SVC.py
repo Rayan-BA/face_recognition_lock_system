9cd42717d4e0f5957e51e94abbfa7fc9d183b9e6
RaychuW <rb.alhamdan@gmail.com>
2024-02-04 16:27:45 +0300

    Clean up and storing with pickle

--- face_recognition_app/SVC.py
+++ face_recognition_app/SVC.py
@@ -1,29 +1,34 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
 import numpy as np
+import pickle
 
 # data = np.load("faces_embeddings.npz")
 # embedded_x = data["arr_0"]
 # y = data["arr_1"]
 # print(y)
 
-data = np.load("faces_embeddings.npz")
-embedded_x, y = data["arr_0"], data["arr_1"]
+def train():
+    data = np.load("faces_embeddings.npz")
+    embedded_x, y = data["arr_0"], data["arr_1"]
 
-encoder = LabelEncoder()
-encoder.fit(y)
-y = encoder.transform(y)
+    encoder = LabelEncoder()
+    encoder.fit(y)
+    y = encoder.transform(y)
 
-x_train, x_test, y_train, y_test = train_test_split(embedded_x, y, shuffle=True)
+    x_train, x_test, y_train, y_test = train_test_split(embedded_x, y, shuffle=True)
 
-model = SVC(kernel="linear", probability=True)
-model.fit(x_train, y_train)
+    model = SVC(kernel="linear", probability=True)
+    model.fit(x_train, y_train)
 
-ypreds_train = model.predict(x_train)
-ypreds_test = model.predict(x_test)
+    with open("SVC_model.pkl", "wb") as f:
+        pickle.dump(model, f)
 
-print(accuracy_score(y_train, ypreds_train))
-print(accuracy_score(y_test, ypreds_test))
+# ypreds_train = model.predict(x_train)
+# ypreds_test = model.predict(x_test)
+
+# print(accuracy_score(y_train, ypreds_train))
+# print(accuracy_score(y_test, ypreds_test))
 
