c5695053491d37d20ad7e4365089f5a82bf204ff
RaychuW <rb.alhamdan@gmail.com>
2024-02-06 19:36:13 +0300

    progress bar

--- face_recognition_app/FaceNetEmbedder.py
+++ face_recognition_app/FaceNetEmbedder.py
@@ -5,7 +5,9 @@ from keras_facenet import FaceNet
 from uuid import uuid1
 from utils import createDir
 
-def extractFace(frame, name, counter):
+faces_embeddings_path = "./models/faces_embeddings.npz"
+
+def extractFace(frame, name, counter, limit):
     face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5, minSize=(100,100))
@@ -14,9 +16,8 @@ def extractFace(frame, name, counter):
         face = frame[y:y+h, x:x+w]
         # face = processFace(face)
         saveFace(face, name)
-        frame = cv.rectangle(frame, (x, y), (x+w, y+h), (50,50,255), 3)
-    cv.imshow("Webcam Capture", frame)
-    cv.setWindowProperty("Webcam Capture", cv.WND_PROP_TOPMOST, 1)
+        cv.rectangle(frame, (x, y), (x+w, y+h), (0,0,255), 2)
+    
     return counter
 
 # def processFace(face, target_size=(160, 160)):
@@ -52,15 +53,33 @@ def loadClasses(dir):
         y.extend(names)
     return np.asarray(x), np.asarray(y)
 
-# Open webcam and do real-time face detection
-def webCamCapture():
+def drawProgBar(frame, w, h, prog):
+    padding = 20
+    prog_bar_start = padding
+    prog_bar_end = w-padding
+
+    cv.rectangle(frame, (prog_bar_start, h-30), (prog_bar_end, h-10), (0,0,255), 2)
+
+    prog_bar = int(prog * prog_bar_end/100)
+    # thickness -1 for fill
+    if prog_bar > padding:
+        cv.rectangle(frame, (prog_bar_start, h-30), (prog_bar, h-10), (0,0,255), -1)
+
+# Open webcam and do real-time face detection and extraction
+def collectFaces():
     name = input("Enter your name: ")
     cam = cv.VideoCapture(0)
     counter = 0
-    limit = 20
+    limit = 100
+    frame_width  = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+    print(frame_width, frame_height)
     while cam.isOpened():
         _, frame = cam.read()
-        counter = extractFace(frame, name, counter)
+        counter = extractFace(frame, name, counter, limit)
+        drawProgBar(frame, frame_width, frame_height, int((counter/limit)*100))
+        cv.imshow("Webcam Capture", frame)
+        cv.setWindowProperty("Webcam Capture", cv.WND_PROP_TOPMOST, 1)
         # ESC to exit
         if cv.waitKey(1) & 0xff == 27 or counter >= limit:
             break
@@ -75,17 +94,13 @@ def getEmbedding(embedder, face_img):
     yhat = embedder.embeddings(face_img)
     return yhat[0]
 
-def collectFaces():
-    webCamCapture()
-
 def createEmbedding():
     print("[INFO] Creating new embeddings...")
     embedder = FaceNet()
-    # webCamCapture()
     x, y = loadClasses("dataset")
     embedded_x = []
     for img in x:
         embedded_x.append(getEmbedding(embedder, img))
     embedded_x = np.asarray(embedded_x)
-    np.savez_compressed("faces_embeddings.npz", embedded_x, y)
+    np.savez_compressed(faces_embeddings_path, embedded_x, y)
     print("[INFO] Embeddings done.")
