f7d9f5c7b51067e695d82c0be89bfdedb06282db
RaychuW <rb.alhamdan@gmail.com>
2024-03-26 02:58:29 +0300

    IT IS ACCURATE

--- face_recognition_app/lbp_antispoof.py
+++ face_recognition_app/lbp_antispoof.py
@@ -1,27 +1,101 @@
 import cv2 as cv
-import numpy as np
-from torch.utils.data import DataLoader
-from torchvision import datasets
+from utils import load_data
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
-face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+face_cascade = cv.CascadeClassifier("./haarcascade_frontalface_default.xml")
 
-def load_data(dataset_path):
-    pass
+def evaluate_model():
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+    import numpy as np
+    from tqdm import tqdm
+
+    x, y = load_data("./antispoof-dataset")
+    
+    encoder = LabelEncoder()
+    encoder.fit(y)
+    y = encoder.transform(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, shuffle=True, random_state=42)
+
+    accuracy_list = []
+    precision_list = []
+    recall_list = []
+    f1_list = []
+    
+    threshold = 70
+    radius = 1
+    neighbors = 8
+    grid_ys = np.arange(2, 9, 2)
+    grid_xs = np.arange(2, 9, 2)
+
+    for grid_y in tqdm(grid_ys):
+        for grid_x in tqdm(grid_xs):
+            recognizer = cv.face.LBPHFaceRecognizer.create(threshold=threshold,radius=radius, neighbors=neighbors, grid_y=grid_y, grid_x=grid_x)
+            recognizer.train(X_train, y_train)
+
+            y_pred = []
+
+            for img in X_test:
+                # Make predictions on the test set
+                y_pred.append(recognizer.predict(img)[0])
+
+            # Calculate accuracy
+            accuracy = accuracy_score(y_test, y_pred)
+            accuracy_list.append(accuracy)
+
+            # Calculate precision
+            precision = precision_score(y_test, y_pred, average="macro")
+            precision_list.append(precision)
+
+            # Calculate recall
+            recall = recall_score(y_test, y_pred, average="macro")
+            recall_list.append(recall)
+
+            # Calculate F1 score
+            f1 = f1_score(y_test, y_pred, average="macro")
+            f1_list.append(f1)
+
+    # best_threshold = thresholds[np.argmax(f1_list)]
+    best_params = (grid_ys[np.argmax(f1_list)], grid_xs[np.argmax(f1_list)])
+
+    # Calculate the average performance metric for each threshold value
+    avg_accuracy = np.mean(accuracy_list)
+    avg_precision = np.mean(precision_list)
+    avg_recall = np.mean(recall_list)
+    avg_f1 = np.mean(f1_list)
+
+    # Print the evaluation metrics
+    print(f"Best parameters: radius={radius}, neighbors={neighbors}, grid_y={best_params[0]}, grid_x={best_params[1]}")
+    print(f"Accuracy: {np.max(accuracy_list):.2f} ({avg_accuracy:.2f} ± {np.std(accuracy_list):.2f})")
+    print(f"Precision: {np.max(precision_list):.2f} ({avg_precision:.2f} ± {np.std(precision_list):.2f})")
+    print(f"Recall: {np.max(recall_list):.2f} ({avg_recall:.2f} ± {np.std(recall_list):.2f})")
+    print(f"F1 Score: {np.max(f1_list):.2f} ({avg_f1:.2f} ± {np.std(f1_list):.2f})")
+
 
 def train():
-    recognizer = cv.face.LBPHFaceRecognizer.create()
     print ("\n [INFO] Training faces. It will take a few seconds. Please wait...")
-    x, y = load_data("antispoof-dataset")
-    recognizer.train(x, y)
+    
+    recognizer = cv.face.LBPHFaceRecognizer.create(radius=1, neighbors=8, grid_x=2, grid_y=2, threshold=70)
+    
+    x, y = load_data("./antispoof-dataset")
+    
+    encoder = LabelEncoder()
+    encoder.fit(y)
+    y = encoder.transform(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, shuffle=True, random_state=42)
+    
+    recognizer.train(X_train, y_train)
     recognizer.write("./models/lbp-antispoof.yml")
+
 
 def recognize():
     print(" [INFO] Recognizing faces...")
-
+    
     recognizer = cv.face.LBPHFaceRecognizer.create()
     recognizer.read("./models/lbp-antispoof.yml")
-    font = cv.FONT_HERSHEY_SIMPLEX
-    x, y = load_data("antispoof-dataset")
+    
     video_capture = cv.VideoCapture(0)
     while video_capture.isOpened():
         ret, frame = video_capture.read()
@@ -31,16 +105,10 @@ def recognize():
         faces = face_cascade.detectMultiScale(frame_gray, minSize = (100, 100))
         for(x,y,w,h) in faces:
             cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-            id, doubt = recognizer.predict(frame_gray[y:y+h,x:x+w])
+            # conf is actually distance, so lower is better
+            pred, conf = recognizer.predict(frame_gray[y:y+h,x:x+w])
             
-            confidence = round(100 - doubt)
-            if confidence > 50:
-                name = y
-            else:
-                name = "Unknown"
-            
-            cv.putText(frame, name, (x+5,y-5), font, 1, (255,255,255), 2)
-            cv.putText(frame, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
+            cv.putText(frame, f"{'real' if pred == 0 else 'spoof'} {str(round(conf))}", (x+5,y-5), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         
         cv.imshow("Recognizer", frame)
         cv.setWindowProperty("Recognizer", cv.WND_PROP_TOPMOST, 1)
@@ -50,4 +118,7 @@ def recognize():
     video_capture.release()
     cv.destroyWindow("Recognizer")
 
-train()
+if __name__ == "__main__":
+    train()
+    # evaluate_model()
+    recognize()
