f53204a1176df53967544364b1c7fca941e0e718
RaychuW <rb.alhamdan@gmail.com>
2024-05-05 21:23:10 +0300

    entries

--- db.py
+++ db.py
@@ -1,25 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
-import datetime
-import bcrypt
+import datetime, bcrypt
 
 db = SQLAlchemy()
 
 class Users(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32))
-    image = db.Column(db.LargeBinary)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
-
     def __init__(self, name, image):
         self.name = name
         self.image = image
+    
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+    image = db.Column(db.LargeBinary)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
 
 class Account(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    password_hash = db.Column(db.String(128), nullable=False)
-
     def __init__(self, password):
         self.set_password(password)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
         self.password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
@@ -28,12 +27,23 @@ class Account(db.Model):
         return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
 
 class Entries(db.Model):
-    entry_number = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
-    accepted = db.Column(db.Boolean, default=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    user = db.relationship('Users', backref=db.backref('entries', lazy=True))
-
-    def __init__(self, user_id):
-        self.user_id = user_id
+    def __init__(self, entry_id, name, time , accepted, image):
+        self.entry_id = entry_id
+        self.name = name
+        self.time = time
+        self.accepted = accepted
+        self.image = image
     
+    entry_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    time = db.Column(db.DateTime)
+    accepted = db.Column(db.Boolean)
+    image = db.Column(db.LargeBinary)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    # user = db.relationship('Users', backref=db.backref('entries', lazy=True))
+
+class EntriesControl(db.Model):
+    def __init__(self, size):
+        self.file_size = size
+
+    file_size = db.Column(db.Integer, primary_key=True)
