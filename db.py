9717975419af9d088784ac6848d66e327a764f05
RaychuW <rb.alhamdan@gmail.com>
2024-05-09 00:39:43 +0300

    added reason for entry rejection

--- db.py
+++ db.py
@@ -27,23 +27,19 @@ class Account(db.Model):
         return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
 
 class Entries(db.Model):
-    def __init__(self, entry_id, name, time , accepted, image):
+    def __init__(self, entry_id, name, time , accepted, image, reject_reason):
         self.entry_id = entry_id
         self.name = name
         self.time = time
         self.accepted = accepted
         self.image = image
+        self.reject_reason = reject_reason
     
     entry_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
     time = db.Column(db.DateTime)
     accepted = db.Column(db.Boolean)
     image = db.Column(db.LargeBinary)
+    reject_reason = db.Column(db.String(32))
     # user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     # user = db.relationship('Users', backref=db.backref('entries', lazy=True))
-
-class EntriesControl(db.Model):
-    def __init__(self, size):
-        self.file_size = size
-
-    file_size = db.Column(db.Integer, primary_key=True)
